2f34c0d6a09f00b33feb7bf8db34a17404260a9e
anaidenko <Andrii.Naidenko@outlook.com>
2020-07-15 02:28:01 +0200

    fix(buttons): add missing constants

---
+++ libs/constants.ts
@@ -0,0 +1,13 @@
+export type Color =
+  | 'primary'
+  | 'secondary'
+  | 'success'
+  | 'danger'
+  | 'warning'
+  | 'info'
+  | 'dark'
+  | 'light'
+  | 'white'
+  | 'muted'
+
+export type Variant = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'dark' | 'light'
