0a24b165486bd2acf5aad98130b62c76583c286f
anaidenko <Andrii.Naidenko@outlook.com>
2020-07-16 15:34:39 +0200

    feat(auth): connect pages to authentication module

--- pages/creditlock.tsx
+++ pages/creditlock.tsx
@@ -2,13 +2,21 @@ import Layout from '@/components/Layout'
 import Header from '@/components/creditlock-page/Header'
 import CreditFreezeCenter from '@/components/creditlock-page/CreditFreezeCenter'
 import FactsSteps from '@/components/creditlock-page/FactsSteps'
+import { useAuthToken } from '@/libs/hooks/use-auth-token'
 
-export const CreditLock = (): JSX.Element => (
-  <Layout>
-    <Header />
-    <CreditFreezeCenter />
-    <FactsSteps />
-  </Layout>
-)
+export const CreditLock = (): JSX.Element => {
+  const [token, { error }] = useAuthToken()
+
+  if (error) return <div>{error.message}</div>
+  if (!token) return <div>Loading...</div>
+
+  return (
+    <Layout>
+      <Header />
+      <CreditFreezeCenter />
+      <FactsSteps />
+    </Layout>
+  )
+}
 
 export default CreditLock
--- pages/id-protection.tsx
+++ pages/id-protection.tsx
@@ -1,16 +1,26 @@
+import { FunctionComponent } from 'react'
+
 import Layout from '@/components/Layout'
 import Header from '@/components/id-protection-page/Header'
 import NotificationInsurance from '@/components/id-protection-page/NotificationInsurance'
 import NotificationLostWallet from '@/components/id-protection-page/NotificationLostWallet'
 import NotificationRestorationServices from '@/components/id-protection-page/NotificationRestorationServices'
+import { useAuthToken } from '@/libs/hooks/use-auth-token'
 
-export const IdProtection = (): JSX.Element => (
-  <Layout>
-    <Header />
-    <NotificationInsurance />
-    <NotificationLostWallet />
-    <NotificationRestorationServices />
-  </Layout>
-)
+export const IdProtection: FunctionComponent = (): JSX.Element => {
+  const [token, { error }] = useAuthToken()
+
+  if (error) return <div>{error.message}</div>
+  if (!token) return <div>Loading...</div>
+
+  return (
+    <Layout>
+      <Header />
+      <NotificationInsurance />
+      <NotificationLostWallet />
+      <NotificationRestorationServices />
+    </Layout>
+  )
+}
 
 export default IdProtection
