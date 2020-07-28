48b08a2d20e21b504a795893ad90e3a29dae5c1c
anaidenko <Andrii.Naidenko@outlook.com>
2020-07-28 17:54:50 +0200

    refactor(api): provide global SWR config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,24 @@
 import { AppProps } from 'next/app'
+import { SWRConfig } from 'swr'
+
+import useFallbackToMock from '@/libs/hooks/use-fallback-to-mock'
 import useFetcher from '@/libs/hooks/use-fetcher'
 
 import '@/styles/index.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  useFetcher()
-  return <Component {...pageProps} />
+  const [fetcher] = useFetcher()
+  useFallbackToMock()
+
+  return (
+    <SWRConfig
+      value={{
+        fetcher,
+        errorRetryCount: 1,
+        revalidateOnFocus: false,
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  )
 }
