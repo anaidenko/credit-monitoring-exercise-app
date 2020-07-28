ea9abcfcca5b1a6d222d59426b3e85701d5ca82b
anaidenko <Andrii.Naidenko@outlook.com>
2020-07-28 23:28:19 +0200

    fix(api): extend api mocks, add short delay, console logs

--- data/mocks/mock-fetcher.ts
+++ data/mocks/mock-fetcher.ts
@@ -1,23 +1,60 @@
+import parse from 'url-parse'
+
 import monitorEnrollments from '@/data/mocks/monitor-enrollments'
+import { EnrollmentCode, MonitorEnrollment } from '@/libs/api/monitoring/getEnrollments'
+import { delay } from '@/utils/promise'
 
 export default {
   instance: async (url: string, params: any) => {
     // eslint-disable-next-line no-console
     console.log('Mocking request...', params.method, url)
 
-    if (url.includes('/api/authenticate/v2') && params.method === 'GET') {
+    const { pathname } = parse(url)
+
+    if (pathname.endsWith('/api/authenticate/v2') && params.method === 'GET') {
       return success(url, { provider: 'tui', questions: [] })
     }
 
-    if (url.includes('/api/monitoring/v2') && params.method === 'GET') {
+    if (pathname.endsWith('/api/monitoring/v2') && params.method === 'GET') {
       return success(url, monitorEnrollments)
+    }
+
+    if (pathname.endsWith('/api/monitoring/v2') && params.method === 'POST') {
+      const newEnrollment: MonitorEnrollment = {
+        provider: 'tui',
+        enrollmentCode: EnrollmentCode.TUICreditLock,
+        active: true,
+        cancellationDate: '',
+        enrollmentDate: new Date().toISOString(),
+      }
+      monitorEnrollments.unshift(newEnrollment)
+      return success(url)
+    }
+
+    if (pathname.endsWith('/api/monitoring/v2') && params.method === 'DELETE') {
+      const newEnrollment: MonitorEnrollment = {
+        provider: 'tui',
+        enrollmentCode: EnrollmentCode.TUICreditLock,
+        active: false,
+        cancellationDate: new Date().toISOString(),
+        enrollmentDate: '',
+      }
+      monitorEnrollments.unshift(newEnrollment)
+      return success(url)
     }
 
     return success(url)
   },
 }
 
-function success(url: string, data = {}) {
+async function success(url: string, data = {}) {
+  await delay(100)
+
+  // eslint-disable-next-line no-console
+  console.log('Mocking response...', 200, data)
+
+  const response = JSON.parse(JSON.stringify(data))
+
   return {
     url,
     ok: true,
@@ -25,7 +62,7 @@ function success(url: string, data = {}) {
     statusText: 'OK',
     headers: {},
     redirected: false,
-    json: async () => data,
-    text: async () => JSON.stringify(data),
+    json: async () => response,
+    text: async () => JSON.stringify(response),
   }
 }
